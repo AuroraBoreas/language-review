18064541bf941114dd8756ecca650a605a4e37e5
AuroraBoreas <AuroraBoreas@users.noreply.github.com>
2021-06-05 14:20:26 +0800

    C++ algorithm

--- 20210523LangReview/C++/review01/meta.cpp
+++ 20210523LangReview/C++/review01/meta.cpp
@@ -28,4 +28,18 @@ void TS::datastructureDemo(void)
 {}
 
 void TS::algorithmDemo(void)
-{}
+{
+    /*
+    @ algorithm
+    ===
+    - 排 permutation
+    - 构 structure changer
+    - 移 mover
+    - 值 value modifier
+    - 集 algo of set
+    - 查 query
+    - 原 raw memory
+    ===
+    */
+
+}
